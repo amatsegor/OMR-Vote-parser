c1b4a553754ab69a347a116debb2d3d728c0d488
amatsegor <amatsegor@autodoc.de>
2017-06-20 16:23:43 +0300

    Index updated

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,8 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const Downloader_1 = require("./Downloader");
+const Parser_1 = require("./Parser");
+const Unzipper_1 = require("./Unzipper");
+const rxjs_1 = require("rxjs");
 /**
  * Created by amatsegor on 6/20/17.
  */
-var Downloader_1 = require("./Downloader");
-exports.default = Downloader_1.Downloader;
+exports.doDaFuckingJob = function (url) {
+    return rxjs_1.Observable.create(observer => {
+        Downloader_1.Downloader.get(url, filePath => {
+            Unzipper_1.Unzipper.unzip(filePath)
+                .subscribe(file => {
+                Parser_1.Parser.parse(file).subscribe(json => observer.next(json));
+            });
+        });
+    });
+};
+var Downloader_2 = require("./Downloader");
+exports.Downloader = Downloader_2.Downloader;
+var Parser_2 = require("./Parser");
+exports.Parser = Parser_2.Parser;
+var Unzipper_2 = require("./Unzipper");
+exports.Unzipper = Unzipper_2.Unzipper;
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import {Downloader} from "./Downloader";
+import {Parser} from "./Parser";
+import {Unzipper} from "./Unzipper";
+import {Observable} from "rxjs";
+/**
+ * Created by amatsegor on 6/20/17.
+ */
+
+export let doDaFuckingJob = function(url: string): Observable<string> {
+    return Observable.create(observer => {
+        Downloader.get(url, filePath => {
+            Unzipper.unzip(filePath)
+                .subscribe(file => {
+                    Parser.parse(file).subscribe(json => observer.next(json))
+                })
+        })
+    })
+};
+
+export {Downloader} from "./Downloader";
+export {Parser} from "./Parser";
+export {Unzipper} from "./Unzipper";
